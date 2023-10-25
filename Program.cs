27de213b3ab80780346f93ed452efbdfb52dd66a
Louise <louiseluoto@hotmail.com>
2023-10-25 15:09:29 +0200

    Created a meny i main/L

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
-﻿namespace Grupparbete_EscapeRoom;
+﻿using System.Diagnostics;
+
+namespace Grupparbete_EscapeRoom;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Nu ska vi leka Escape Room");
+    {   // Här ska vi göra en meny.
+        bool isRunning = true;
+        while (isRunning)
+        {
+            Console.WriteLine("1. Play");
+            Console.WriteLine("2. Exit");
+            string choice = Console.ReadLine()+"";
+            switch (choice)
+            {
+                case "1":
+
+                    break;
+                case "2":
+                    isRunning = false;
+                    Console.WriteLine("Tack och hej leverpastej!");
+                    break; 
+            }
+        }
     }
 }
