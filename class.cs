8adf41e87294bb2b8c0256d399822bc2b530c27a
Sandra <sandy.andersson.6@gmail.com>
2023-10-26 09:17:42 +0200

    Player class och roomclass /S

--- class.cs
+++ class.cs
@@ -1,4 +1,22 @@
-class Room1
+using System.ComponentModel;
+
+public class Room
 {
+    public string QuestionText {get; set;}
     
+    public Room(string questiontext)
+    {
+        QuestionText = questiontext;
+    }
+}
+
+class FreeText : Room
+{
+    public string? FreeTextAnswer {get; set;}
+
+    public FreeText(string questiontext, string freeTextAnswer) : base(questiontext)
+    {
+        FreeTextAnswer = freeTextAnswer;
+    }
+
 }
